781b7745e780a931b08656ce9cbfaf0adaac61aa
ST10084348 <ST10084348@vcconnect.edu.za>
2023-10-07 15:55:39 +0000

    Initial Commit

---
+++ server/db/conn.mjs
@@ -0,0 +1,20 @@
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+dotenv.config();
+
+const variable = process.env.MONGO_CONN_STRING
+console.log(variable);
+
+const client = new MongoClient(variable);
+
+let conn;
+try {
+  conn = await client.connect();
+  console.log('MongoDB is Successfully Connected!');
+} catch(e) {
+  console.error(e);
+}
+
+let db = client.db("users");
+
+export default db;
